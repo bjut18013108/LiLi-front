c16af062dbff9c01790b24f9c18e9393f486b5ff
bjut18013108 <w15810560768@163.com>
2022-03-30 10:16:59 +0800

    Finished order page

--- pages/orders/orders.js
+++ pages/orders/orders.js
@@ -43,29 +43,54 @@ Page({
                 price: "8.5",
                 cdkey: "LL888",
             }
-        ]
+        ],
+        showList: []
     },
 
-
+    // 页面创建时执行
     onLoad: function (options) {
-
+        this.setData({showList: this.data.orderList})
     },
 
-
+    // 页面出现在前台时执行
     onShow: function () {
 
     },
 
     // 筛选显示全部订单
     selectAll: function() {
-
+        console.log("筛选显示全部");
+        var localShowList = [];
+        for (let i=0; i<this.data.orderList.length; i++) {
+            localShowList.push(this.data.orderList[i])
+        }
+        this.setData({showList : localShowList})
     },
 
     // 筛选显示进行中订单，包括备餐中与待取餐
-    selectAll: function() {
-
+    selectInProgress: function() {
+        console.log("筛选显示进行中");
+        var localShowList = [];
+        for (let i=0; i<this.data.orderList.length; i++) {
+            if (this.data.orderList[i].state === "备餐中" ||
+                this.data.orderList[i].state === "待取餐") {
+                localShowList.push(this.data.orderList[i])
+            }
+        }
+        this.setData({showList : localShowList})
     },
 
-    
+    // 筛选显示已结束订单，包括已完成和已过期
+    selectFinished: function() {
+        console.log("筛选显示已结束");
+        var localShowList = [];
+        for (let i=0; i<this.data.orderList.length; i++) {
+            if (this.data.orderList[i].state === "已完成" ||
+                this.data.orderList[i].state === "已过期") {
+                localShowList.push(this.data.orderList[i])
+            }
+        }
+        this.setData({showList : localShowList})
+    },
 
 })
