a82bc4be4f515faa9f0a65cc9d7cbdad95e8f08e
WenYihan <w15810560768@163.com>
2022-04-11 18:41:40 +0800

    Finished detail page

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -6,63 +6,28 @@ Page({
      */
     data: {
         goodsName:"鸡丁黄瓜盖饭套餐",
+        ID:"F0000001",
         price: 10.2,
-        time: "19:30 - 20:00"
+        time: "19:30 - 20:00",
+        detailList: [
+            {
+                detail: "1. 两荤一素, 菜品随机，二两米饭共600克。"
+            },
+            {
+                detail: "2. 请确保在规定时间内取餐，过时订单自动取消。多次购买不取将影响信誉值！"
+            },
+            {
+                detail: "3. 不退不换。"
+            }
+        ]
     },
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
+    purchase : function (e) {
+        let goodsID = e.currentTarget.dataset.name;
+        console.log("selected goods ID: "+goodsID);
+        // wx.navigateTo({
+        //     url: '/pages/shop/shop'
+        // })
     }
+
 })
